035c2d17e9b5d165b7f874f8289e1b47bc501f93
Exjobb <exjobb2011@.stockholm.cint.com>
2011-09-14 14:46:48 +0200

    trying to commit things

--- MyPhotoBlog/Modules/RootModule.cs
+++ MyPhotoBlog/Modules/RootModule.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
-using Nancy;
-using Simple.Data;
 using MyPhotoBlog.Services;
 
 namespace MyPhotoBlog.Modules
